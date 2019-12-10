b09ab7d7f6e8b7f689dd15793fe6ada2616fe25d
SorinGabriel02 <lupu_sg@yahoo.com>
2019-12-10 17:52:45 +0200

    fetch posts and users

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
 import postReducer from "./postReducer";
+import usersReducer from "../reducers/usersReducer";
 
-export default combineReducers({ posts: postReducer });
+export default combineReducers({ posts: postReducer, users: usersReducer });
